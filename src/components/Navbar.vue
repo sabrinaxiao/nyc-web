b33499cf1aed60ad24e162d875589d5929dc41dc
sabrinaxiao <sabrinaxiao01@gmail.com>
2020-06-09 19:33:57 -0400

    first commit - made a toolbox

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav>
+        <v-toolbar flat app class="change-font">
+            <v-img
+            :src="require('@/assets/pastel.jpg')"
+            :height = "70">
+            <v-toolbar-title class="text-uppercase blue--text font display-1">
+                <span class="font-weight-light">NYC</span>
+                <span> Hot Spots</span>
+            </v-toolbar-title>
+            </v-img>
+        </v-toolbar>
+    </nav>
+</template>
+<script>
+
+export default {
+    
+}
+</script>
