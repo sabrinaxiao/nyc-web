3b574b3c7ec9a2242725a3138cb0ef3434a711db
sabrinaxiao <sabrinaxiao01@gmail.com>
2020-06-10 00:03:09 -0400

    fixed toolbar and added beginning of navigation drawer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,20 +1,25 @@
 <template>
     <nav>
-        <v-toolbar flat app class="change-font">
-            <v-img
-            :src="require('@/assets/pastel.jpg')"
-            :height = "70">
-            <v-toolbar-title class="text-uppercase blue--text font display-1">
-                <span class="font-weight-light">NYC</span>
-                <span> Hot Spots</span>
+        <v-toolbar flat app>
+            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-toolbar-title class="text-uppercase purple--text">
+                <span class="font-weight-bold font display-1">nyc </span>
+                <span class ="text-uppercase pink--text font-weight-light title"> Hot Spots</span>
             </v-toolbar-title>
-            </v-img>
         </v-toolbar>
+
+        <v-navigation-drawer v-model="drawer" app class="indigo">
+            <p>test</p>
+        </v-navigation-drawer>
     </nav>
 </template>
-<script>
 
+<script>
 export default {
-    
+    data(){
+        return{
+            drawer: false
+        }
+    }
 }
 </script>
