e5f88bc401e9f917c2dd90388ac1ec58dccfbae1
sabrinaxiao <sabrinaxiao01@gmail.com>
2020-06-13 22:56:23 -0400

    finished parks and restaurant tabs

--- src/views/Park.vue
+++ src/views/Park.vue
@@ -2,7 +2,77 @@
   <div class="park">
     <h1 class= "headline">Parks</h1>
     <v-container class="my-5">
-      content
+      <v-list-item href="https://www.centralpark.com/" class="my-4">
+          <v-img height="400" src="../assets/park-1.jpg" contain class="my-5"></v-img> 
+      </v-list-item>
+      <h1 class="text-center headline">Central Park</h1>
+      <v-layout col wrap justify-space-around >
+        <v-flex xs10 md6>
+          <h1 class="subtitle-1 my-3"> Central Park is the largest park in New York City, stretching 51 blocks and separating the Upper West and Upper East parts of the city. It is a little nature getaway, while still being surrounded by skyscrapers on either side of the park. There are beautiful arches and bridges scattered all around and big lawns for picnicking with loved ones. There is even a castle embedded deep in the park. You can visit the Central Park Zoo or ride the carousel or paddle down the lake in a wooden boat or even ride on a horse drawn carriage. The best time to visit is cherry blossom season, when the park would have endless rows of cherry blossom trees and pink petals fluttering down.  </h1>
+          <h1 class="subtitle-1 font-weight-bold my-2">Must see: </h1>
+          <h1 class="subtitle-1 "> Shakespeare in the Park. Every year, the Delacorte Theater holds free large scale Shakespeare productions in the middle of the park.  </h1>
+        </v-flex>
+        <v-flex xs10 md6>
+          <iframe class="mx-4 my-4" width="500" height="400" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/view?zoom=17&center=40.7678%2C-73.9718&key=AIzaSyDH4BbFyezDzLM8Z2jHUMpJmkvhH6t3AXE" allowfullscreen></iframe>
+          <h1 class="subtitle-1 mx-4 font-weight-bold my-2">Transportation: </h1>
+          <h1 class="subtitle-1 mx-4">Since Central Park is so big, there are many forms of transportation. The bus or subway line vary based on which part of the park you want to go to.</h1>
+          <h1 class="subtitle-1 mx-4">  <v-icon>directions_bus</v-icon> Bus:  M1, M2, M3, M4, M10, M66, M72, M79, M86, M96, M106 </h1> 
+          <h1 class="subtitle-1 mx-4"> <v-icon>train</v-icon> Train: PATH  </h1>
+          <h1 class="subtitle-1 mx-4"> <v-icon>subway</v-icon> Subway: 1, 2, 3, 4, 5, 6, A, B, C, D, F, N, Q, R, W </h1>
+          </v-flex>
+        </v-layout> 
+        <v-list-item href="https://www.bbg.org/" class="my-4">
+          <v-img height="400" src="../assets/park-2.jpg" contain class="my-5"></v-img> 
+      </v-list-item>
+      <h1 class="text-center headline">Brooklyn Botanic Garden</h1>
+      <v-layout col wrap justify-space-around >
+        <v-flex xs10 md6>
+          <h1 class="subtitle-1 my-3"> Brooklyn Botanic Garden is the best botanical garden in the city and has nearly a million visitors annually. There are beautiful gardens and lawns featuring over 18,000 types of plants from all over the world. You can sit in a gazebo at the Japanese Hill and Pond Garden, overlooking the koi fish swimming beneath you or walk through endless rows of rainbow colored roses at the Cranford Rose Garden. The main attraction every year is their Cherry Blossom Festival, Sakura Matsuri. There are marital arts and musical performances showing off Japanese culture, as well as tea ceremonies. This garden is breathtaking, whether you go in the spring or in winter.  </h1>
+          <h1 class="subtitle-1 font-weight-bold my-2">Must see: </h1>
+          <h1 class="subtitle-1 "> Conservatory. Their conservatory grows bonsai and orchids, as well as having three pavilions- desert, tropical, and warm temperate. </h1>
+        </v-flex>
+        <v-flex xs10 md6>
+          <iframe class="mx-4 my-4" width="500" height="400" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/view?zoom=17&center=40.6694%2C-73.9624&key=AIzaSyDH4BbFyezDzLM8Z2jHUMpJmkvhH6t3AXE" allowfullscreen></iframe>
+          <h1 class="subtitle-1 mx-4 font-weight-bold my-2">Transportation: </h1>
+          <h1 class="subtitle-1 mx-4">  <v-icon>directions_bus</v-icon> Bus:  B16, B41, B43, B45, B48, B49, B65, B69 </h1> 
+          <h1 class="subtitle-1 mx-4"> <v-icon>train</v-icon> Train: LIRR </h1>
+          <h1 class="subtitle-1 mx-4"> <v-icon>subway</v-icon> Subway: 2, 3, 4, 5, B, Q, S </h1>
+          </v-flex>
+        </v-layout> 
+         <v-list-item href="https://govisland.com/" class="my-4">
+          <v-img height="400" src="../assets/park-3.jpg" contain class="my-5"></v-img> 
+      </v-list-item>
+      <h1 class="text-center headline">The Hills at Govenors Island</h1>
+      <v-layout col wrap justify-space-around >
+        <v-flex xs10 md6>
+          <h1 class="subtitle-1 my-3"> Governor’s Island is a mini getaway from the hustle and bustle of the city. The Hill is one of the best views of the Statue of Liberty and offers plenty of scenic views for photography opportunities. The island has been recently opened to the public, for it has been reconstructed from its original use of being a military base for the US Army and the Coast Guard. Besides the few eerie looking houses left behind, there are bike hills, galleries, and gardens residing next to a historic castle. You can even ride a surrey with friends while circling the promenade. Governor’s Island is only open from May 1 to October 31 and you can only get there by ferry. </h1>
+          <h1 class="subtitle-1 font-weight-bold my-2">Must see: </h1>
+          <h1 class="subtitle-1 "> Castle Williams, a 200 year old building that was used to be a military fortress and army prison. </h1>
+        </v-flex>
+        <v-flex xs10 md6>
+          <iframe class="mx-4 my-4" width="500" height="400" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/view?zoom=15&center=40.6895%2C-74.0168&key=AIzaSyDH4BbFyezDzLM8Z2jHUMpJmkvhH6t3AXE" allowfullscreen></iframe>
+          <h1 class="subtitle-1 mx-4 font-weight-bold my-2">Transportation: </h1>
+          <h1 class="subtitle-1 mx-4">  <v-icon>directions_boat</v-icon> Ferry from Battery Maritime Building </h1> 
+          </v-flex>
+        </v-layout> 
+        <v-list-item href="https://parks.ny.gov/parks/gantryplaza/details.aspx" class="my-4">
+          <v-img height="400" src="../assets/park-4.jpg" contain class="my-5"></v-img> 
+        </v-list-item>
+        <h1 class="text-center headline">Gantry Plaza State Park </h1>
+        <v-layout col wrap justify-space-around >
+          <v-flex xs10 md6>
+            <h1 class="subtitle-1 my-3"> This park’s location was recently renovated from being a dockyard and manufacturing district and became a rapidly growing residential and commercial neighborhood with thriving art culture. Gantry Plaza State Park's most prominent feature is its 120 feet long neon ruby colored Pepsi-Cola sign. The park is surrounded by breathtaking views of the East River and the skyline from Midtown Manhattan. With plenty of benches and lawns for picnicking, this park is the perfect place to lay back while enjoying a beautiful view.  </h1>
+            <h1 class="subtitle-1 font-weight-bold my-2">Must see: </h1>
+            <h1 class="subtitle-1 "> Fireworks. If you visit during July 4th, you will have the best view of the Macy’s 4th of July Fireworks show without having to deal with a big crowd blocking your view.  </h1>
+          </v-flex>
+          <v-flex xs10 md6>
+            <iframe class="mx-4 my-4" width="500" height="400" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/view?zoom=17&center=40.7455%2C-73.9587&key=AIzaSyDH4BbFyezDzLM8Z2jHUMpJmkvhH6t3AXE" allowfullscreen></iframe>
+            <h1 class="subtitle-1 mx-4 font-weight-bold my-2">Transportation: </h1>
+            <h1 class="subtitle-1 mx-4">  <v-icon>directions_bus</v-icon> Bus:  B62, Q103, Q32, Q67 </h1> 
+            <h1 class="subtitle-1 mx-4"> <v-icon>train</v-icon> Train: PORT WASHINGTON </h1>
+            <h1 class="subtitle-1 mx-4"> <v-icon>subway</v-icon> Subway: 7, E, G </h1>
+          </v-flex>
+        </v-layout> 
     </v-container>
   </div>
 </template>
